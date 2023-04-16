f6bcfb9468ad3761884c33ec02777b31de109390
mitsuYashi <hm20011027@gmail.com>
2023-04-16 12:04:15 +0900

    feat: routing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,3 +2,4 @@ export { createRoom } from "./features/createRoom";
 export { joinRoom } from "./features/joinRoom";
 export { leaveRoom } from "./features/leaveRoom";
 export { setMemberReadyState } from "./features/setMemberReadyState";
+export { submitAnswer } from "./features/submitAnswer";
