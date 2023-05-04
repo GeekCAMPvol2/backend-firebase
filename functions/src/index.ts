a83254f25e2c5c1828b61801150515a8b633ea13
nnsyobo <nnsyobo@gmail.com>
2023-05-04 20:53:49 +0900

    add "src/recievers/index.ts" to simplify importing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,1 @@
-export { createRoom } from "./recievers/createRoom";
-export { joinRoom } from "./recievers/joinRoom";
-export { leaveRoom } from "./recievers/leaveRoom";
-export { setMemberReadyState } from "./recievers/setMemberReadyState";
+export * from "./recievers";
---
+++ functions/src/recievers/index.ts
@@ -0,0 +1,5 @@
+export { createRoom } from "./createRoom";
+export { joinRoom } from "./joinRoom";
+export { leaveRoom } from "./leaveRoom";
+export { setMemberReadyState } from "./setMemberReadyState";
+export { submitAnswer } from "./submitAnswer";
